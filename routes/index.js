118788cf1be7d79be3e2c0326525282e4defc948
brokenfinga <brokenfinga@gmail.com>
2015-08-17 16:58:26 +0300

    Second change

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Itai perez & Elad ventura' });
+  res.render('index', { title: 'Shaul' });
 });
 
 module.exports = router;
